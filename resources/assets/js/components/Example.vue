b8fe1443f7c29813bee5d6008b44db95cb01c382
Edward Lance Lorilla <edwardlorilla1986@gmail.com>
2017-06-05 20:59:30 +0800

    Login with axios

--- resources/assets/js/components/Example.vue
+++ resources/assets/js/components/Example.vue
@@ -3,10 +3,48 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Example Component</div>
-
+                    <div class="panel-heading">Login</div>
                     <div class="panel-body">
-                        I'm an example component!
+                        <form class="form-horizontal" role="form">
+
+                            <div class="form-group">
+                                <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+
+                                <div class="col-md-6">
+                                    <input id="email" type="email" class="form-control" name="email" v-model="loginDetails.email" required autofocus>
+                                </div>
+                            </div>
+
+                            <div class="form-group }}">
+                                <label for="password" class="col-md-4 control-label">Password</label>
+
+                                <div class="col-md-6">
+                                    <input id="password" type="password" class="form-control" v-model="loginDetails.password" name="password" required>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-6 col-md-offset-4">
+                                    <div class="checkbox">
+                                        <label>
+                                            <!--<input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me-->
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="form-group">
+                                <div class="col-md-8 col-md-offset-4">
+                                    <button type="submit" @click.prevent="loginPost" class="btn btn-primary">
+                                        Login
+                                    </button>
+
+                                    <a class="btn btn-link" href="">
+                                        Forgot Your Password?
+                                    </a>
+                                </div>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -15,9 +53,31 @@
 </template>
 
 <script>
+
     export default {
+
+        data(){
+            return{
+                loginDetails:{
+                    email:'',
+                    password:''
+                }
+            }
+        },
+        methods:{
+        loginPost(){
+            let vm = this;
+            axios.post('/login', vm.loginDetails)
+            .then(function (response) {
+            console.log(response);
+            })
+            .catch(function (error) {
+            console.log(error.response);
+            });
+        }
+        },
         mounted() {
-            console.log('Component mounted.')
+
         }
     }
 </script>
